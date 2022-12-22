dbbd717091bfc53e03ac160fae5ebf2a2dd05578
anmolbhalla98 <84401873+anmolbhalla98@users.noreply.github.com>
2022-12-22 23:00:14 +0530

    Task Programme.

---
+++ Task.java
@@ -0,0 +1,11 @@
+public class Task{
+    public static void main(String args[]){
+        float a=2345f;
+        a+=8;
+        a/=3;
+        a%=5;
+        a*=5;
+        System.out.println("The Final Value of A"+"="+a);
+        //The Final Value of A=21.666565
+    }
+}
