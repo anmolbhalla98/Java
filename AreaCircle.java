2ccd42f41bc76de8737822a9b0497d24f7418fdc
anmolbhalla98 <84401873+anmolbhalla98@users.noreply.github.com>
2022-12-22 22:35:28 +0530

    Area of Circle Programme.

---
+++ AreaCircle.java
@@ -0,0 +1,9 @@
+public class AreaCircle{
+    public static void main(String args[]){
+        float Area=0f;
+        float Radius=3.2f;
+        float Pi=3.14f;
+        Area=Pi*(Radius*Radius);
+        System.out.println("The Area of Circle is"+" = "+Area);
+    }
+}
---
+++ Driving.java
@@ -0,0 +1,11 @@
+public class Driving{
+    public static void main(String args[]){
+        int a=10;
+        if(a<=18){
+            System.out.println("Not Eligible for Driving Licese Because ur age is"+" =  "+a);
+            }
+            else{
+                System.out.println("You are Eligible for Driving License");
+            }
+            }
+    }
