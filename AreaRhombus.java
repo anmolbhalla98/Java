0ac254f34c7a4721c7cb8c59d842ab6a92c28d63
anmolbhalla98 <84401873+anmolbhalla98@users.noreply.github.com>
2022-12-22 22:47:10 +0530

    Area Of Rhombus Programme.

---
+++ AreaRhombus.java
@@ -0,0 +1,9 @@
+public class AreaRhombus{
+    public static void main(String args[]){
+float Area;
+float Diagonal1=10f;
+float Diagonal2=10f;
+Area=(Diagonal1*Diagonal2)/2;
+System.out.println("The Area of Rhombus whose diagonals are known "+" "+Area);
+    }
+}
